013c01990e691af87158b03d9886f0044387929b
Wichaphon <horokindov3310@gmail.com>
2025-10-07 20:20:12 +0700

    fixed fileFilter error handler

--- backend/src/middlewares/upload.middleware.js
+++ backend/src/middlewares/upload.middleware.js
@@ -26,7 +26,7 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
-    cb(new Error("อนุญาตเฉพาะไฟล์รูปภาพเท่านั้น (JPG, PNG, WEBP)"), false);
+    cb(new Error("Only image files are allowed (JPG, PNG, WEBP)"), false);
   }
 };
 
