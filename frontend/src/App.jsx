28e6edd38a5a39ac91b7181e70187e629e9cb397
Wichaphon <horokindov3310@gmail.com>
2025-10-08 19:06:09 +0700

    Added setup vite react project + Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    
+    <div className="bg-blue-500 text-white p-4 font-bold text-4xl">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
