fa3e3292b7a084806bf6b1d976711f7df3a9ad05
Wichaphon <horokindov3310@gmail.com>
2025-10-22 00:51:03 +0700

    Create endpoint to fetch pending seller applications

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,2 +1,32 @@
 import prisma from "../lib/prisma.js";
 
+export const getPendingSellers = async (req, res) => {
+    try {
+        const pendingSellers = await prisma.sellerProfile.findMany({
+            where: { status: 'PENDING' },
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        email: true,
+                        firstName: true,
+                        lastName: true,
+                        avatar: true,
+                        createdAt: true,
+                    },
+                },
+            },
+            orderBy: { createdAt: 'asc' }, //เก่าสุดก่อน
+        });
+
+        res.json({
+            count: pendingSellers.length,
+            sellers: pendingSellers,
+        });
+    }
+
+    catch (error) {
+        console.error(`Get pending sellers error: ${error} | from adminController`);
+        res.status(500).json({ error: 'Failed to get pending sellers.' });
+    }
+};
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { authenticate, authorize } from '../middlewares/auth.middleware.js';
+import { getPendingSellers } from '../controllers/admin.controller.js';
 
 const router = express.Router();
 
 router.use(authenticate, authorize('ADMIN'));
+
+//Seller Management
+router.get("/sellers/pending", getPendingSellers);
+
 
 export default router;
